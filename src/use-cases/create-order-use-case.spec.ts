245ddb3939dafe06012e1c8f55da9b9deedf50f5
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-05-08 19:27:01 -0300

    refact: set randomUUID for create products

--- src/use-cases/create-order-use-case.spec.ts
+++ src/use-cases/create-order-use-case.spec.ts
@@ -1,21 +1,22 @@
+import { randomUUID } from 'node:crypto'
 import { Product } from '../entities/Product'
-import { InMemoryOrderRepository } from '../repositories/in-memory/in-memory-orders-repository'
+import { InMemoryOrdersRepository } from '../repositories/in-memory/in-memory-orders-repository'
 import { CreateOrderUsecase } from './create-order-use-case'
 
-let orderRepository: InMemoryOrderRepository
+let orderRepository: InMemoryOrdersRepository
 let sut: CreateOrderUsecase
 
 describe('Create Order )UseCase', () => {
     beforeEach(() => {
-        orderRepository = new InMemoryOrderRepository()
+        orderRepository = new InMemoryOrdersRepository()
         sut = new CreateOrderUsecase(orderRepository)
     })
 
     it('should ble able create an order with 3 products', async () => {
         const products: Product[] = [
-            { amount: 1, price: 100, description: 'Product 1' },
-            { amount: 2, price: 200, description: 'Product 2' },
-            { amount: 3, price: 100, description: 'Product 3' }
+            { id: randomUUID(), amount: 1, price: 100, description: 'Product 1' },
+            { id: randomUUID(), amount: 2, price: 200, description: 'Product 2' },
+            { id: randomUUID(), amount: 3, price: 100, description: 'Product 3' }
         ]
         const { order } = await sut.execute({
             description: 'Order 1',
