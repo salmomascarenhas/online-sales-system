be8c0bf5b09688d56469c79012183329aef6309d
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-05-08 16:55:29 -0300

    feat: make create-order-usecase

---
+++ src/core/use-cases/create-order-use-case.ts
@@ -0,0 +1,33 @@
+import { Order } from '../entities/Order'
+import { OrdersRepository } from '../repositories/orders-repository'
+import { Product } from '../entities/Product'
+import { Ticket } from '../entities/Ticket'
+
+
+interface CreateOrderUseCaseRequest {
+    description: string
+    products: Product[]
+    ticket: Ticket | null
+}
+
+interface CreateOrderUseCaseReponse {
+    order: Order
+}
+
+export class CreateOrderUsecase {
+    constructor(private ordersRepository: OrdersRepository) { }
+
+    async execute({
+        description,
+        products,
+        ticket
+    }: CreateOrderUseCaseRequest): Promise<CreateOrderUseCaseReponse> {
+        const order = await this.ordersRepository.create({
+            description,
+            products,
+            ticket
+        })
+
+        return { order }
+    }
+}
