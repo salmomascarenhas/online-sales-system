e2853042440212c821bf2d2e17faa5a7e2fc127a
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-05-08 17:15:25 -0300

    refact: set id if does not exists in data param

--- src/repositories/in-memory/in-memory-orders-repository.ts
+++ src/repositories/in-memory/in-memory-orders-repository.ts
@@ -1,11 +1,13 @@
-import { Order } from '@/core/entities/Order'
+import { Order } from '@/entities/Order'
 import { OrdersRepository } from '../orders-repository'
+import { randomUUID } from 'crypto'
 
 export class InMemoryOrderRepository implements OrdersRepository {
     public items: Order[] = []
 
     async create(data: Order): Promise<Order> {
         const order: Order = {
+            id: data.id ?? randomUUID(),
             description: data.description,
             products: data.products,
             ticket: data.ticket
