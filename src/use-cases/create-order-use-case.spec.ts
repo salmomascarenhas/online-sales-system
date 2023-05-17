e2a35ef9bdd7250ce994fede9b88250c25016eb1
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-05-16 21:27:27 -0300

    test: ensure createOrderUseCase should not be able create order with invalid cpf

--- src/use-cases/create-order-use-case.spec.ts
+++ src/use-cases/create-order-use-case.spec.ts
@@ -12,7 +12,7 @@ describe('Create Order )UseCase', () => {
         sut = new CreateOrderUsecase(orderRepository)
     })
 
-    it('should ble able create an order with 3 products', async () => {
+    it('should be able create an order with 3 products', async () => {
         const products: Product[] = [
             { id: randomUUID(), amount: 1, price: 100, description: 'Product 1' },
             { id: randomUUID(), amount: 2, price: 200, description: 'Product 2' },
@@ -21,9 +21,24 @@ describe('Create Order )UseCase', () => {
         const { order } = await sut.execute({
             description: 'Order 1',
             products,
-            discountCoupon: null
+            discountCoupon: null,
+            cpf: '364.303.290-03'
         })
         expect(order.products).toHaveLength(3)
         expect(order).toHaveProperty('id')
+    })
+
+    it('should not be able create order with invalid cpf', async () => {
+        const products: Product[] = [
+            { id: randomUUID(), amount: 1, price: 100, description: 'Product 1' },
+            { id: randomUUID(), amount: 2, price: 200, description: 'Product 2' },
+            { id: randomUUID(), amount: 3, price: 100, description: 'Product 3' }
+        ]
+        await expect(sut.execute({
+            description: 'Order 1',
+            products,
+            discountCoupon: null,
+            cpf: '364.303.290-00'
+        })).rejects.toThrowError('Invalid CPF')
     })
 })
