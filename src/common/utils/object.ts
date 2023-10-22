5122be58af7c9c93f68166487ded726d87957e52
salmomascarenhas <salmo.cruz@gmail.com>
2023-10-22 15:32:49 -0300

    feat: create deepFreeze object

---
+++ src/common/utils/object.ts
@@ -0,0 +1,16 @@
+export function deepFreeze<T>(obj: T) {
+    try {
+      const propNames = Object.getOwnPropertyNames(obj);
+      for (const name of propNames) {
+        const value = obj[name as keyof T];
+  
+        if (value && typeof value === 'object') {
+          deepFreeze(value);
+        }
+      }
+      return Object.freeze(obj);
+    } catch (e) {
+      return obj;
+    }
+  }
+  
